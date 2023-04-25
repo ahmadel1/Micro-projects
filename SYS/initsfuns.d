b016ed0fc8f452aa87e2b9689d61cf55d490452f
Ahmed Adel <73443649+ahmadel1@users.noreply.github.com>
2023-04-25 06:49:06 +0200

    Delete SYS directory

--- SYS/initsfuns.d
+++
@@ -1,4 +0,0 @@
-.\initsfuns.o: inits\initsFuns.c
-.\initsfuns.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\initsfuns.o: inits\initsHeaders.h
-.\initsfuns.o: inits\tm4c123gh6pm.h
--- SYS/main.d
+++
@@ -1,4 +0,0 @@
-.\main.o: main.c
-.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\main.o: inits\initsHeaders.h
-.\main.o: inits\tm4c123gh6pm.h
